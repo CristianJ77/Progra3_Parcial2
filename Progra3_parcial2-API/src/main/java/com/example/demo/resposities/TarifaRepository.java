5b325b739e3064beec7a1de0a951c0592a6a515e
Cristian Figueroa <Cristian Figueroa@192.168.0.107>
2021-04-25 22:50:42 -0600

    Parcial2 finalizado

---
+++ Progra3_parcial2-API/src/main/java/com/example/demo/resposities/TarifaRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.resposities;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.entities.Tarifa;
+
+public interface TarifaRepository extends CrudRepository<Tarifa, Long> {
+
+}
